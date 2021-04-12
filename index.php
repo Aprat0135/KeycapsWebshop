556ef776f673f9daf8987fbadf602429dc5ea27d
Philipp <pm2001patra@gmail.com>
2021-04-12 13:19:08 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+    <?php
+        include 'head.html';
+    ?>
+    <body>
+        <header>
+	        <?php
+		        include 'inc/nav.html';
+	        ?>
+	    </header>
+        <?php
+            include 'darkmode-switch.html';
+        ?>
+        <main>
+            <h1>index.php</h1>
+        </main>
+        <?php
+            include 'inc/footer.html';
+	    ?> 
+    </body>
+</html>
